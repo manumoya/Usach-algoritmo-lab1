272acde01501fafb0aaa3d498e9d625ee7e4cb8c
Manuel Moya <mmoyam@gmail.com>
2017-10-09 13:16:06 -0300

    agregando typo Cola y Nodo
    agregando typo Cola y Nodo

--- cola.c
+++ cola.c
@@ -1,46 +1,70 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h> 
 
+/*
 struct _nodo {
 	char valor[20];
 	struct _nodo *siguiente;
 };
+*/
 
-/*  FUNCIONES DE COLA  */
-struct _nodo *primero, *ultimo;
 
+typedef struct _nodo {
+    char valor[20];
+    struct _nodo *siguiente;
+}Nodo;
+
+typedef struct _cola{
+  Nodo *inicio;
+  Nodo *final;  
+}Cola;
+
+/*  FUNCIONES DE COLA  */ 
+
+
+void inicializa(Cola *cola){
+  cola->inicio = NULL;
+  cola->final = NULL; 
+}
+  
 /* cola push */
-void push(char valor[20]) {
+void push(Cola *cola, char valor[20]) {
 	/* Crear un nodo nuevo */
-  struct _nodo *nuevo;
-  nuevo = (struct _nodo*)malloc(sizeof(struct _nodo));
+  Nodo *nuevo;
+  nuevo = (Nodo*)malloc(sizeof(Nodo));
 
   strcpy(nuevo->valor,valor);
   /* Este será el último nodo, no debe tener siguiente */
   nuevo->siguiente = NULL;
 
   /* Si primero es NULL, la cola estaba vacía, ahora primero apuntará también al nuevo nodo */
-  if(primero==NULL)
-    primero = nuevo;
+  printf("\n Acá pushh ini: %s", nuevo->valor);
+  if(cola->inicio==NULL){
+    printf("\nAcá 1ro:\n");
+    cola->inicio = nuevo;
+    printf("\n Acá pushh 1ro: %s", cola->inicio->valor);
+  
+  }
+
   /* Si la cola no estaba vacía, añadimos el nuevo a continuación de ultimo */
-  if(ultimo!=NULL)
-    ultimo->siguiente = nuevo;
-   	
+  if(cola->final!=NULL)
+    cola->final->siguiente = nuevo;
+  
   /* Ahora, el último elemento de la cola es el nuevo nodo */
-  ultimo = nuevo;
+  cola->final= nuevo;
 }
 
 /* cola pop */
-void pop() {
-  struct _nodo *nodo_pop;
-  if (primero==NULL){
+void pop(Cola *cola) {
+  Nodo *nodo_pop;
+  //if (primero==NULL){
+  if (cola->inicio==NULL){
     printf( "\nLa lista está vacía!! (pop)\n" );
     return;
   }  
-  nodo_pop = primero;
-  primero = nodo_pop->siguiente;
+  nodo_pop = cola->inicio;
+  cola->inicio = nodo_pop->siguiente;
   
   printf("pop primero: %s", nodo_pop->valor);
   printf("\n");
@@ -48,9 +72,9 @@ void pop() {
 }
 
 /* Imprimir cola */
-void print_list(){
-	struct _nodo *auxiliar;
-	auxiliar = primero;
+void print_cola(Cola *cola){
+	Nodo *auxiliar;
+	auxiliar = cola->inicio;
     printf("\nMostrando la lista completa:\n");
     if (auxiliar==NULL) printf( "\nLa lista está vacía!!\n" );
     while (auxiliar!=NULL) {
@@ -63,7 +87,8 @@ void print_list(){
 
 
 
-/*  FUNCIONES DE MATRIZ  */
+/*  ======= FUNCIONES DE MATRIZ  ========== */
+int largo =5;
 char matriz_grafo[5][5];
 
 /* generar grafo */
@@ -80,7 +105,12 @@ void generar_grafo(char vertices[]) {
 void imprimir_grafo(){
   for (int i=0; i<5; i++){
     for (int j=0; j<5; j++){
-      printf("%c ", matriz_grafo[i][j]);
+      if (j==0 || i==0){
+        printf("%c ", matriz_grafo[i][j]);
+      }else{
+        printf("%d ", matriz_grafo[i][j]);
+      }
+      
     }
     printf("\n");   
   }
@@ -102,22 +132,53 @@ int posicion_nodo(char letra[]){
   return posicion;
 }
 
+void agregar_adyacencia(char origen[], char destino[], int costo){
+  int pos_origen=posicion_nodo(origen);
+  int pos_destino=posicion_nodo(destino); 
+
+  printf("pos orig %d ", pos_origen ); 
+  printf("pos dsti %d ", pos_destino ); 
+  printf("\n");   
+
+
+  matriz_grafo[pos_origen][pos_destino]=costo;
+
+}
+
 int main() {
-	push("A");
-	push("B");
-  push("C");
-  print_list();
-  pop();
-  print_list();
-  push("D");
-  print_list();
-  pop();
-  print_list();
-  pop();
+  //struct _nodo *primero, *ultimo;
+  //primero=NULL;
+  //ultimo=NULL;
+
+  Cola *cola;
+  if ((cola = (Cola *) malloc (sizeof (Cola))) == NULL) 
+    return -1;
+  inicializa(cola);
+  push(cola,"A");
+  //printf("\n Acá main: %s", cola->inicio->valor);
+
+	push(cola,"B");
+  push(cola,"C");
+  
+  //printf("\n Acá main: %s", cola);
+  
+
+  print_cola(cola);
+  
+  pop(cola);
+  pop(cola);
+  print_cola(cola);
+  
+  /*
+  push(cola,"D");
+  print_list(cola);
+  pop(cola);
+  print_list(cola);
+  pop(cola);
   
   generar_grafo("abcd");
   imprimir_grafo();
-  printf("pos c %d ", posicion_nodo("c") ); 
-  printf("pos a %d ", posicion_nodo("a") ); 
-
+  agregar_adyacencia("a", "b", 1);
+  imprimir_grafo();
+*/
 }
